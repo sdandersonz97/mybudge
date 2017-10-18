8dfa5d11ec74f4bd29b7a08d666f78b2ab8001f4
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 22:46:44 -0400

    feat: controllers

---
+++ app.js
@@ -0,0 +1,21 @@
+//BUDGET CONTROLLER
+var budgeController = (function(){
+    
+    
+    
+    })();
+    
+    
+    //UI CONTROLLER
+    var UIController = (function(){
+    
+    
+    
+    })();
+    
+    
+    //GLOBAL CONTROLLER
+    var controller = (function(budgeCtrl, UICtrl){
+        
+    
+    })(budgeController, UIController);
