4116ed8828e99aa46b83dde37641b6ba38c67bcc
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 23:19:21 -0400

    feat: event listener for addItem

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ var budgeController = (function(){
     
     
     
-    })();
+})();
     
     
-    //UI CONTROLLER
-    var UIController = (function(){
+//UI CONTROLLER
+var UIController = (function(){
+
+
+
+})();
     
     
-    
-    })();
-    
-    
-    //GLOBAL CONTROLLER
-    var controller = (function(budgeCtrl, UICtrl){
-        
-    
-    })(budgeController, UIController);
+//GLOBAL CONTROLLER
+var controller = (function(budgeCtrl, UICtrl){
+
+    var ctrlAddItem  = function() {
+        console.log('record')
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+
+})(budgeController, UIController);
